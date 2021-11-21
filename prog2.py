1e5475025adf3fa1e48195ab916928964e57912b
unknown <christineilagan02@gmail.com>
2021-11-22 01:33:43 +0800

    Program 2: Addition Quiz code is almost done

--- prog2.py
+++ prog2.py
@@ -18,8 +18,9 @@ def display_intro():
         print("You can now exit.")
     else:
         print("Please enter yes or no.")
-        return answer
 
+
+total = 10
 
 def begin():
     title = "** Tine's Addition Quiz **"
@@ -31,8 +32,8 @@ def begin():
 def display_menu():
      menu_list = ["1. Start", "2. Practice", "3. Exit"]
      print(menu_list[0])
+     print(menu_list[1])
      print(menu_list[2])
-     print(menu_list[3])
 
 
 def display_separator():
@@ -46,6 +47,48 @@ def get_user_input():
     else:
         return user_input
 
+def get_user_solution(problem):
+    print("Enter your answer")
+    print(problem, end="")
+    result = int(input(" = "))
+    return result
+
+def check_solution(user_solution, solution, count):
+    if user_solution == solution:
+        count = count + 1
+        print("Correct!")
+        return count
+    else:
+        print("Incorrect :(")
+        return count
+
+
+def menu_option(index, count):
+    for problem in range(1, 11):
+        number_one = random.randrange(0, 99)
+        number_two = random.randrange(0, 99)
+        if index == 1:
+            problem = str(number_one) + " + " + str(number_two)
+            solution = number_one + number_two
+            user_solution = get_user_solution(problem)
+            count = check_solution(user_solution, solution, count)
+            return count
+        else:
+            problem = str(number_one) + " + " + str(number_two)
+            solution = number_one + number_two
+            user_solution = get_user_solution(problem)
+            count = check_solution(user_solution, solution, count)
+            return count
+
+def display_result(correct, wrong):
+    if correct >= 10 and wrong == 0:
+        print(f"Your score is {correct}/{total} ")
+    elif correct >= 5 or correct == 9 and wrong >= 1:
+        print(f"Your score is {correct}/{total} ")
+    elif correct >= 1 or correct == 4 and wrong >= 1:
+        print(f"Your score is {correct}/{total} ")
+    else:
+        print(f"Your score is {correct}/{total} ")
 
 
 def menu():
@@ -53,9 +96,17 @@ def menu():
     begin()
     display_menu()
     display_separator()
-    get_user_input()
 
+    option = get_user_input()
+    total = 0
+    correct = 0
+    while option != 3:
+        total = total + 1
+        correct = menu_option(option, correct)
+        option = get_user_input()
 
+    print("Exit the quiz.")
+    display_separator()
 
 menu()
 
